80edd632a303377ebab64c794ac26b336cab0315
egublog <egukei0729@gmail.com>
2021-11-29 22:33:28 +0900

    [ADD] Stateパターンの実装

---
+++ BehavioralPatterns/State/State.php
@@ -0,0 +1,71 @@
+<?php 
+
+interface WritingState
+{
+    public function write(string $words);
+}
+
+class UpperCase implements WritingState
+{
+    public function write(string $words)
+    {
+        echo strtoupper($words);
+    }
+}
+
+class LowerCase implements WritingState
+{
+    public function write(string $words)
+    {
+        echo strtolower($words);
+    }
+}
+
+class DefaultType implements WritingState
+{
+    public function write(string $words)
+    {
+        echo $words;
+    }
+}
+
+class TextEditor
+{
+    protected $state;
+
+    public function __construct(WritingState $state)
+    {
+        $this->state = $state;
+    }
+
+    public function setState(WritingState $state)
+    {
+        $this->state = $state;
+    }
+
+    public function type(string $words)
+    {
+        $this->state->write($words);
+    }
+}
+
+$editor = new TextEditor(new DefaultType());
+
+$editor->type('First line');
+
+$editor->setState(new UpperCase());
+
+$editor->type('Second line');
+$editor->type('Third line');
+
+$editor->setState(new LowerCase());
+
+$editor->type('Fourth line');
+$editor->type('Fifth line');
+
+// 出力:
+// First line
+// SECOND LINE
+// THIRD LINE
+// fourth line
+// fifth line
