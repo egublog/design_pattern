02d7af4fd139f6d926733169c5fd799674200409
egublog <egukei0729@gmail.com>
2022-05-23 15:47:23 +0900

    [CREATE] CreationalPatterns/Prototype/java/Manager.java

---
+++ CreationalPatterns/Prototype/java/Manager.java
@@ -0,0 +1,17 @@
+package CreationalPatterns.Prototype.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Manager {
+  private Map<String, Product> showcase = new HashMap<>();
+
+  public void register(String name, Product product) {
+    showcase.put(name, product);
+  }
+
+  public Product create(String prototypeName) {
+    Product p = showcase.get(prototypeName);
+    return p.createClone();
+  }
+}
