fdb59b7c343b09a3c0a911e7c19062cb1df4504f
egublog <egukei0729@gmail.com>
2022-05-29 23:39:35 +0900

    [MODIFY] Prototypeパターン

--- CreationalPatterns/Prototype/Prototype.php
+++ CreationalPatterns/Prototype/Prototype.php
@@ -1,44 +1,27 @@
 <?php
-// 適当にクラスを作成
-class Sheep
-{
-  protected $name;
-  protected $category;
-
-  public function __construct(string $name, string $category = 'オオツノヒツジ')
-  {
-    $this->name = $name;
-    $this->category = $category;
-  }
-
-  public function setName(string $name)
-  {
-    $this->name = $name;
-  }
-
-  public function getName()
-  {
-    return $this->name;
-  }
-
-  public function setCategory(string $category)
-  {
-    $this->category = $category;
-  }
-
-  public function getCategory()
-  {
-    return $this->category;
-  }
+class Status {
+  public int $level;
 }
 
-// クローンを試してみる
-$original = new Sheep('ジョリー');
-echo $original->getName();     // ジョリー
-echo $original->getCategory(); // オオツノヒツジ
+class Player {
+  public string $name;
+  public Status $status;
+}
 
-// クローン後、必要なものを変更する
-$cloned = clone $original;
-$cloned->setName('ドリー');
-echo $cloned->getName();     // ドリー
-echo $cloned->getCategory(); // オオツノヒツジ
+
+$status = new Status();
+$status->level = 20;
+
+$player1 = new Player();
+$player1->name = "佐藤";
+$player1->status = $status;
+
+
+$player2 = $player1;
+
+$player2->name = "田中";
+$player2->status->level = 10000;
+
+
+var_dump($player1); // 名前は田中、レベルは10000
+var_dump($player2); // 名前は田中、レベルは10000
